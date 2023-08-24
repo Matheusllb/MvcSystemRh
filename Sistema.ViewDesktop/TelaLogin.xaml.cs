541f5dee0a4fd972d4ed34775fc181fc45d45b8f
Matheus Batista <matheusllbatista@outlook.com>
2023-08-23 23:50:30 -0300

    configuracoes

---
+++ Sistema.ViewDesktop/TelaLogin.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Sistema.ViewDesktop
+{
+    /// <summary>
+    /// Lógica interna para TelaLogin.xaml
+    /// </summary>
+    public partial class TelaLogin : Window
+    {
+        public TelaLogin()
+        {
+            InitializeComponent();
+        }
+    }
+}
