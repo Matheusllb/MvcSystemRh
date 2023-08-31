edc799bbfe4f18b400bfaac5f656e082346ab311
Guilherme Andreta <guilherme.andretasantos@hotmail.com>
2023-08-30 22:03:36 -0300

    Classes BeneficioDescontoDAO e EmpresaDAO criadas

--- Sistema.Model/DAO/BeneficioDescontoDAO.cs
+++ Sistema.Model/DAO/BeneficioDescontoDAO.cs
@@ -1,16 +1,69 @@
-﻿using Microsoft.Analytics.Interfaces;
-using Microsoft.Analytics.Types.Sql;
+﻿using Internal;
+using System.Data;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Types.Sql;
+using Sistema.Model.Entidades;
 
 namespace Sistema.Model.DAO
 {
-    internal class BeneficioDesconto
+    public class BeneficioDescontoDAO
     {
+        private DbConnectionManager connectionManager;
 
+        public BeneficioDescontoDAO()
+        {
+            connectionManager = new DbConnectionManager();
+        }
+
+        public List<BeneficioDesconto> GetAllBeneficiosDescontos()
+        {
+            List<BeneficioDesconto> beneficiosDescontos = new List<BeneficioDesconto>();
+
+            using (SqlConnection connection = connectionManager.GetConnection())
+            {
+                string query = "SELECT IdBeneficioDesconto, Descricao, Desconto, Valor FROM BeneficioDesconto";
+                SqlCommand command = new SqlCommand(query, connection);
+
+                try
+                {
+                    connectionManager.OpenConnection();
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        BeneficioDesconto beneficioDesconto = new BeneficioDesconto
+                        {
+                            IdBeneficioDesconto = Convert.ToInt32(reader["IdBeneficioDesconto"]),
+                            Descricao = reader["Descricao"].ToString(),
+                            Desconto = Convert.ToBoolean(reader["Desconto"]),
+                            Valor = Convert.ToDouble(reader["Valor"])
+                        };
+
+                        beneficiosDescontos.Add(beneficioDesconto);
+                    }
+
+                    reader.Close();
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine("An error occurred: " + ex.Message);
+                }
+
+                finally
+                {
+                    connectionManager.CloseConnection();
+                }
+            }
+
+            return beneficiosDescontos;
+        }
     }
 }
 
